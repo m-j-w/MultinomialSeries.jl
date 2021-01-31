9f1db0b8593a77d64fbbb841423c23db64b816c5
Markus J. Weber <m-w4@gmx.de>
2021-01-31 20:05:14 +0100

    Added testsets for eachmultinomial

--- test/test_multinomials.jl
+++ test/test_multinomials.jl
@@ -6,31 +6,66 @@
 
 #=----    Reference Sinusoid Solver Tests    ----=#
 
-#@testset "Multinomial" begin
+@testset "Standard Multinomials" begin
 
-    function collect_multinomial(m,n)
-        a = []
-        add_term(x,y) = (print("."); push!(a, (x,y)))
-        multinomial_generator(add_term, m, n)
-        return a
+    @test collect(eachmultinomial(1,0)) == [(0,)]
+    @test collect(eachmultinomial(2,2)) == [(2,0), (1,1), (0,2)]
+    @test collect(eachmultinomial(2,3)) == [(3,0), (2,1), (1,2), (0,3)]
+    @test collect(eachmultinomial(3,3)) == [
+                (3, 0, 0), (2, 1, 0), (2, 0, 1), (1, 2, 0), (1, 1, 1),
+                (1, 0, 2), (0, 3, 0), (0, 2, 1), (0, 1, 2), (0, 0, 3) ]
+
+end
+
+
+@testset "Automated consistency checks" begin
+
+    for (m,n) in [ (1,0), (1,1), (1,2),
+                   (2,2), (2,3), (2,4), (2,4),
+                   (3,0), (3,1), (3,2), (3,3), (3,4), (3,5),
+                   (4,0), (4,1), (4,2), (4,3), (4,4), (4,5),
+                   (8,0), (8,1), (8,2), (8,3), (8,4), (8,5), 
+                   (8,6), (8,7), (8,8), (8,9), (8,10), (8,12)
+                ]
+
+        it = eachmultinomial(m,n)
+        xs = collect(it)
+        @test   length(first(xs)) == m          # tuple length correct
+        @test   length(xs) == length(it)        # theoretical number reached
+        @test   all( sum(x) == n for x in xs )  # all add up to n
+        @test   all( all(x .>= 0) for x in xs )  # all non-negative
+        @test   unique(xs) == xs                # no duplicates in list
+
+    end
+            
+end # testset
+
+
+@testset "Result types" begin
+
+    for t in (UInt8, Int8, UInt16, Int16, UInt32, Int32, UInt64, Int64, UInt128, Int128)
+
+        @test collect(eachmultinomial(t, 1, 1))[1] isa Tuple{t}
+        @test collect(eachmultinomial(t, 2, 2))[2] isa Tuple{t,t}
+        @test collect(eachmultinomial(t, 3, 3))[3] isa Tuple{t,t,t}
+    
+        @test eltype(collect(eachmultinomial(t, 1, 1))) == Tuple{t}
+        @test eltype(eachmultinomial(t, 1, 1)) == Tuple{t}
     end
 
-    # specific tests for well known cases
-    # 1) (a,b)²
-    xs = collect_multinomial(2,2)
-    println(xs)
-    # 2) (a,b,c)³
+end
 
 
-    # generic consistency checks:
-    # 1) number of resulting terms matches ?
-    # 2) sum of all coefficients matches ?
-    # 3) no duplicated coefficient packs
+@testset "Wrong function arguments" begin
 
+    @test_skip #=DomainError=# collect(eachmultinomial( 0, 0))
+    @test_skip #=DomainError=# collect(eachmultinomial(-1, 0))
+    @test_skip #=DomainError=# collect(eachmultinomial( 1,-1))
 
-    #@test ...
+    @test_skip #=TypeError=# collect(eachmultinomial(String, 1, 1))  # Only integer types
+    @test_skip #=TypeError=# collect(eachmultinomial(BigInt, 1, 1))  # Only bits types, sorry.
 
-#end # testset
+end
 
 
 # end of file
